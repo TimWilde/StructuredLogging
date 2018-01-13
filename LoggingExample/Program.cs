2be1755916aa3cd7f7b6c5affb27f73d4f9aed24
Tim Wilde <tim@timwil.de>
2018-01-13 03:17:12 +0000

    Little structured logging demo from NLog to ELK.

---
+++ LoggingExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.StructuredLogging.Json;
+
+namespace LoggingExample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            LogManager.Configuration = new XmlLoggingConfiguration("nlog.config");
+
+            var logger = LogManager.GetLogger(typeof(Program).FullName);
+
+            logger.Info("Hello World!");
+            logger.ExtendedInfo("Extended Greetings!", new {WordOfTheDay = "Jibble"});
+
+            logger.Factory.Flush();
+        }
+    }
+}
